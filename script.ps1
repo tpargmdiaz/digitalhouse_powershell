7d721b84cf70c95e8fc469ef285f96651b036f62
Mauricio Diaz <tpargmdiaz@gmail.com>
2022-03-15 19:44:50 +0000

    tercer commit

--- script.ps1
+++ script.ps1
@@ -1,3 +1,5 @@
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/olea/lemarios/master/nombres-propios-es.txt -OutFile lista_nombres.txt
 foreach($nombre in $(Get-Content -Path .\lista_nombres.txt)) {
 Write-Output "El nombre es $nombre"
 }
